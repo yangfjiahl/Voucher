935c48abe20128cfdab88cc151e1665dce003896
zhuhaipeng <dell@qq.com>
2019-03-06 08:55:33 +0800

    update alipay 未完成

--- app/src/main/java/com/mandou/voucher/MainActivity.java
+++ app/src/main/java/com/mandou/voucher/MainActivity.java
@@ -99,36 +99,6 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    /**
-     * do alipay req
-     *
-     * @param msg
-     */
-    private void alipay(Message msg){
-
-        @SuppressWarnings("unchecked")
-        PayResult payResult = new PayResult((Map<String, String>) msg.obj);
-
-
-        /**
-         对于支付结果，请商户依赖服务端的异步通知结果。同步通知结果，仅作为支付结束的通知。
-         */
-        String resultInfo = payResult.getResult();// 同步返回需要验证的信息
-        String resultStatus = payResult.getResultStatus();
-
-        // 判断resultStatus 为9000则代表支付成功
-        if (TextUtils.equals(resultStatus, "9000")) {
-
-            // 该笔订单是否真实支付成功，需要依赖服务端的异步通知。
-            Toast.makeText(getApplication(), "支付成功", Toast.LENGTH_SHORT).show();
-
-        } else {
-
-            // 该笔订单真实的支付结果，需要依赖服务端的异步通知。
-            Toast.makeText(getApplication(), "支付失败", Toast.LENGTH_SHORT).show();
-
-        }
-    }
 
     private void wechatPay(Bundle bundle) {
         JSONObject data = (JSONObject) bundle.getSerializable("data");
@@ -237,64 +207,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    /**
-     * 初始化支付宝支付
-     *
-     * @param appId
-     */
-    private void initAliPay(String appId) {
-
-        // 初始化aplipay
-
-        Button button = findViewById(R.id.btn_pay_alipay);
-        button.setVisibility(View.VISIBLE);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendAliPayRequest();
-            }
-        });
-    }
-
-
-    /**
-     * alipay req
-     */
-    public void sendAliPayRequest(){
-//        boolean rsa2 = (RSA2_PRIVATE.length() > 0);
-//        Map<String, String> params = OrderInfoUtil2_0.buildOrderParamMap(APPID, rsa2);
-//        String orderParam = OrderInfoUtil2_0.buildOrderParam(params);
-//
-//        String privateKey = rsa2 ? RSA2_PRIVATE : RSA_PRIVATE;
-//        String sign = OrderInfoUtil2_0.getSign(params, privateKey, rsa2);
-//        final String orderInfo = orderParam + "&" + sign;
-
-        // OTDO:
-        final String orderInfo = "";
-
-        Runnable payRunnable = new Runnable() {
-
-            @Override
-            public void run() {
-                PayTask alipay = new PayTask(MainActivity.this);
-                Map<String, String> result = alipay.payV2(orderInfo, true);
-
-                Log.i(TAG, result.toString());
-
-                Message msg = new Message();
-                msg.what = MSG_PAY_ALIPAY;
-                msg.obj = result;
-                handler.sendMessage(msg);
-            }
-        };
-
-        Thread payThread = new Thread(payRunnable);
-        payThread.start();
-    }
-
-
-
     public void sendWechatPayRequest() {
         String amountStr = amount.getText().toString();
         String bizNoStr = bizNo.getText().toString();
@@ -352,4 +264,99 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    /**
+     * 初始化支付宝支付
+     *
+     * @param appId
+     */
+    private void initAliPay(String appId) {
+
+        // 初始化aplipay
+
+        Button button = findViewById(R.id.btn_pay_alipay);
+        button.setVisibility(View.VISIBLE);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendAliPayRequest();
+            }
+        });
+    }
+
+
+    /**
+     * alipay req
+     *
+     */
+    public void sendAliPayRequest(){
+
+//        boolean rsa2 = (RSA2_PRIVATE.length() > 0);
+//        Map<String, String> params = OrderInfoUtil2_0.buildOrderParamMap(APPID, rsa2);
+//        String orderParam = OrderInfoUtil2_0.buildOrderParam(params);
+//
+//        String privateKey = rsa2 ? RSA2_PRIVATE : RSA_PRIVATE;
+//        String sign = OrderInfoUtil2_0.getSign(params, privateKey, rsa2);
+
+//        final String orderInfo = orderParam + "&" + sign;
+
+        // OTDO:
+        final String orderInfo = "";
+
+        Runnable payRunnable = new Runnable() {
+
+            @Override
+            public void run() {
+                PayTask alipay = new PayTask(MainActivity.this);
+                Map<String, String> result = alipay.payV2(orderInfo, true);
+
+                Log.i(TAG, result.toString());
+
+                Message msg = new Message();
+                msg.what = MSG_PAY_ALIPAY;
+                msg.obj = result;
+                handler.sendMessage(msg);
+            }
+        };
+
+        Thread payThread = new Thread(payRunnable);
+        payThread.start();
+    }
+
+    /**
+     * do alipay
+     *
+     * @param msg
+     */
+    private void alipay(Message msg){
+
+        @SuppressWarnings("unchecked")
+        PayResult payResult = new PayResult((Map<String, String>) msg.obj);
+
+
+        /**
+         对于支付结果，请商户依赖服务端的异步通知结果。同步通知结果，仅作为支付结束的通知。
+         */
+        String resultInfo = payResult.getResult();// 同步返回需要验证的信息
+        String resultStatus = payResult.getResultStatus();
+
+        // 判断resultStatus 为9000则代表支付成功
+        if (TextUtils.equals(resultStatus, "9000")) {
+
+            // 该笔订单是否真实支付成功，需要依赖服务端的异步通知。
+            Toast.makeText(getApplication(), "支付成功", Toast.LENGTH_SHORT).show();
+
+        } else {
+
+            // 该笔订单真实的支付结果，需要依赖服务端的异步通知。
+            Toast.makeText(getApplication(), "支付失败", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+
+
+
 }
