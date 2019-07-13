73cd769f4200bc9fba468afdec28a3328eb84e3a
zhuhaipeng <dell@qq.com>
2019-07-13 14:47:13 +0800

    update session

--- app/src/main/java/com/mandou/voucher/Session.java
+++ app/src/main/java/com/mandou/voucher/Session.java
@@ -9,28 +9,4 @@ import android.os.Build;
 public class Session {
 
 
-    /**
-     * 获取手机厂商名称
-     * @return
-     */
-    public String getDeviceBrand(){
-        return Build.BRAND;
-    }
-
-    /**
-     * 获取手机型号
-     * @return
-     */
-    public String getDeviceModel(){
-        return Build.MODEL;
-    }
-
-    /**
-     * 获取当前系统版本号
-     * @return
-     */
-    public String getSysVersion(){
-        return Build.VERSION.RELEASE;
-    }
-
 }
